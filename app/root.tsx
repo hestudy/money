9a22399e8f73e3036385d65ea71fa0435a02e58a
hestudy <1467602180@qq.com>
2025-05-13 13:50:37 +0800

    ✨ feat(root.tsx): 添加ScrollRestoration组件 ✨ feat(vite.config.ts): 添加code-inspector-plugin插件

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="h-screen w-screen overflow-hidden">
+      <body className="h-screen w-screen">
         {children}
         <Toaster />
         <ScrollRestoration />
